73b7851e85bb89cbd7d5752d313c295be8d70fc3
viborotto <vittoriaborotto@gmail.com>
2020-03-10 23:29:54 -0300

    Criado operacao de instanciacao

--- cursomc/src/main/java/com/cursomc/cursomc/CursomcApplication.java
+++ cursomc/src/main/java/com/cursomc/cursomc/CursomcApplication.java
@@ -1,13 +1,30 @@
 package com.cursomc.cursomc;
 
+import com.cursomc.cursomc.domain.Categoria;
+import com.cursomc.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+
+		//salvar no banco
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+	}
 }
