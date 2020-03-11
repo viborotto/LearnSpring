1cd8533acd5ba94b02df3a0cd810d231ba18e917
viborotto <vittoriaborotto@gmail.com>
2020-03-10 23:05:09 -0300

    Conectado e configurado ao H2

--- cursomc/src/main/java/com/cursomc/cursomc/domain/Categoria.java
+++ cursomc/src/main/java/com/cursomc/cursomc/domain/Categoria.java
@@ -1,9 +1,16 @@
 package com.cursomc.cursomc.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
+@Entity
 public class Categoria implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
